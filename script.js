c63eedea651662d3ad285f3ac9759eda7215d4fa
varunvk4041 <varunvk4041@gmail.com>
2024-03-10 16:06:10 +1100

    Add index.html, styles.css and script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(){
+    var textsArray = ["Melbourne","Sydney", "Perth", "Canberra", "Adelaide"]
+    var number = getRandomNumberBetween(0,textsArray.length - 1)
+    console.log("Index: ", number)
+    document.getElementById("heading").innerHTML = textsArray[number];
+    }
+    
+function getRandomNumberBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    
